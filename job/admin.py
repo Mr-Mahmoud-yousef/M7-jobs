1ff349ceb77b1ae44f48ed60c207319e3d0ee9fc
User <mohamadmostfa869@gmail.com>
2020-10-05 17:06:08 +0300

    add job models

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import job
+admin.site.register(job)
--- job/models.py
+++ job/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+JOP_TYPE=(
+        ('Full Time','Full Time'),
+        ('Part Time','Part Time'),
+        
+)
+class vacancy_places(models.IntegerChoices):
+    One=1
+    Two=2
+    Three=3
+    Four=4
+    Five=5
+    Sex=6
+    Seven=7
+class job(models.Model):
+    title=models.CharField(max_length=100)
+    #location
+    Job_TYPE=models.CharField(max_length=30 ,choices = JOP_TYPE)
+    Discripion=models.TextField(max_length=600)
+    Published_at=models.TimeField(auto_now=True)
+    vacancy=models.IntegerField(choices=vacancy_places.choices)
+    salery=models.IntegerField(default=000)
+    Experience=models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+    
